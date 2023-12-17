b68eb0d82129014e79a8a7e2d88c02afc0fe8694
Abdelrahman El-Badawy <abdoo738@yahoo.com>
2023-12-17 03:35:56 +0200

    general/feat/#10/request_message_to_server: Added Server Post Request to Send it to openAi
    general/feat/#10/request_message_to_server: Added Server Post Request to Send it to OpenAI
    
    In this commit, a significant feature is introduced to facilitate communication with the OpenAI server. A Server Post Request mechanism has been implemented, enabling the application to send user-generated messages to the OpenAI API. The commit includes the integration of QNetworkAccessManager to handle the HTTP POST request, along with the necessary logic to process server responses.
    
    This feature lays the groundwork for seamlessly interacting with OpenAI's services, marking progress towards enhancing the application's capabilities for natural language processing. The commit addresses the feature request #10.

--- LAA/mainwindow.h
+++ LAA/mainwindow.h
@@ -3,7 +3,12 @@
 
 #include "qpushbutton.h"
 #include <QMainWindow>
-
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QUrlQuery>
+#include <QMessageBox>
+#include <QScrollBar>
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -21,8 +26,11 @@ public:
 private slots:
     void on_pushButtonNewChat_clicked();
 
-
+    void onGetNetworkReply(QNetworkReply *reply);
     // void onButtonClickedThread();
+    void on_pushButtonSendReq_clicked();
+    void onNetworkReply(QNetworkReply *reply);
+
 private:
     Ui::MainWindow *ui;
     std::vector<QPushButton*> buttonsThread;
