fd74abfc17c3e4795d887755890007f2324b7317
Abdelrahman El-Badawy <abdoo738@yahoo.com>
2023-12-21 17:31:48 +0200

    general/feat/#14/view_chats_from_db: added class conversation functionality

--- LAA/conversation.cpp
+++ LAA/conversation.cpp
@@ -1,7 +1,13 @@
 #include "conversation.h"
+#include "qdebug.h"
+#include "qsqlerror.h"
+#include "qsqlquery.h"
 
 // Constructor
-Conversation::Conversation(const QString& id) : id(id) {}
+Conversation::Conversation(const QString& id, const QString& lawerId, const int& qtThreadId, const QSqlDatabase dbChatConnection)
+    : id(id), lawerId(lawerId), qtThreadId(qtThreadId) {
+    // Additional initialization if needed
+}
 
 // Getter for id
 const QString& Conversation::getId() const {
@@ -11,4 +17,57 @@ const QString& Conversation::getId() const {
 // Setter for id
 void Conversation::setId(const QString& newId) {
     id = newId;
+}
+
+// Getter for lawerId
+const QString& Conversation::getLawerId() const {
+    return lawerId;
+}
+
+// Setter for lawerId
+void Conversation::setLawerId(const QString& newLawerId) {
+    lawerId = newLawerId;
+}
+
+// Getter for qtThreadId
+const int& Conversation::getQtThreadId() const {
+    return qtThreadId;
+}
+
+// Setter for qtThreadId
+void Conversation::setQtThreadId(const int& newQtThreadId) {
+    qtThreadId = newQtThreadId;
+}
+bool Conversation::saveToDatabase() {
+    QSqlQuery query(dbChatConnection);
+    query.prepare("INSERT INTO Thread (id, Lawyer_id, Qt_thread_id) VALUES (:id, :lawyerId, :conversationId)");
+    query.bindValue(":id", id);
+    query.bindValue(":lawyerId", lawerId);
+    query.bindValue(":conversationId", qtThreadId);
+
+    if (query.exec()) {
+        qDebug() << "Chat saved to database!";
+        return true;
+    } else {
+        qDebug() << "Error saving chat to database:" << query.lastError().text();
+        return false;
+    }
+}
+
+bool Conversation::loadFromDatabase(const int& threadId) {
+    QSqlQuery query(dbChatConnection);
+    query.prepare("SELECT * FROM Thread WHERE Qt_thread_id = :conversationId");
+    query.bindValue(":conversationId", threadId);
+
+    if (query.exec() && query.next()) {
+        id = query.value("id").toString();
+        lawerId = query.value("lawyerId").toString();
+        qtThreadId = query.value("Qt_thread_id").toInt();
+        QSqlDatabase::database().commit();
+        qDebug() << "Chat loaded from database!";
+        return true;
+    } else {
+        qDebug() << "Error loading chat from database:" << query.lastError().text();
+        return false;
+    }
 }
--- LAA/conversation.h
+++ LAA/conversation.h
@@ -1,21 +1,39 @@
 #ifndef CONVERSATION_H
 #define CONVERSATION_H
 
+#include "qsqldatabase.h"
 #include <QString>
 
 class Conversation {
 private:
     QString id;
-
+    QString lawerId;
+    int qtThreadId;
+    QSqlDatabase dbChatConnection;
 public:
     // Constructor
-    Conversation(const QString& id);
+    Conversation(const QString& id, const QString& lawerId, const int& qtThreadId,const QSqlDatabase dbChatConnection);
 
     // Getter for id
     const QString& getId() const;
 
     // Setter for id
     void setId(const QString& newId);
+
+    // Getter for lawerId
+    const QString& getLawerId() const;
+
+    // Setter for lawerId
+    void setLawerId(const QString& newLawerId);
+
+    // Getter for qtThreadId
+    const int& getQtThreadId() const;
+
+    // Setter for qtThreadId
+    void setQtThreadId(const int& newQtThreadId);
+    bool saveToDatabase();
+
+    bool loadFromDatabase(const int& threadId) ;
 };
 
 #endif // CONVERSATION_H
